8acd6425803f5310d1bea3173781217517edfc19
titi42111 <titi42.maitre@gmail.com>
2025-10-24 13:03:29 +0200

    fix: close prisma with nest shutdown hooks

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,15 +1,22 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
 
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+
   async enableShutdownHooks(app: INestApplication): Promise<void> {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+    app.enableShutdownHooks();
   }
 }
